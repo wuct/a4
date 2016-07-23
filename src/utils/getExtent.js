8b7e32b7501db4ef76dc0cd11da4a611d09404b1
wuct <wu.chingting@gmail.com>
2016-07-23 16:12:49 +0800

    Support for getExtent

---
+++ src/utils/getExtent.js
@@ -0,0 +1,4 @@
+const getExtent = arr =>
+  [Math.min(...arr), Math.max(...arr)]
+
+export default getExtent
---
+++ src/utils/test/getExtent.test.js
@@ -0,0 +1,8 @@
+import { test } from 'ava'
+import getExtent from '../getExtent'
+
+test('getExtent', t => {
+  const extent = getExtent([1, 1.5, 19, 42, -4, 0])
+
+  t.deepEqual(extent, [-4, 42])
+})
