6e136b703cbfa6d3cb5189aeebe317f314e19da9
wuct <wu.chingting@gmail.com>
2016-11-14 17:49:36 +0800

    export translate

--- modules/utils/translate.js
+++ modules/utils/translate.js
@@ -1,5 +1,4 @@
-const translate = (x, y) => `translate(${x}, ${y})`
-
-export default translate
+export const translate = (x, y) => `translate(${x}, ${y})`
 export const translateX = x => translate(x, 0)
 export const translateY = y => translate(0, y)
+export default translate
